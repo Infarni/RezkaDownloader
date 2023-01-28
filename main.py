8e429b7d348df5f90fa66750fa3f54e9f905c5f9
Infarni <archdroider@proton.me>
2023-01-29 00:17:05 +0200

    Remove relative path

--- main.py
+++ main.py
@@ -12,17 +12,6 @@ from webdriver_manager.chrome import ChromeDriverManager
 from handlers import config, rezka_api, video
 
 
-def resource_path(relative_path):
-    """ Get absolute path to resource, works for dev and for PyInstaller """
-    try:
-        # PyInstaller creates a temp folder and stores path in _MEIPASS
-        base_path = sys._MEIPASS
-    except Exception:
-        base_path = os.path.abspath(".")
-
-    return os.path.join(base_path, relative_path)
-
-
 def main():
     # Create data dir
     if not os.path.exists(config.PATH):
